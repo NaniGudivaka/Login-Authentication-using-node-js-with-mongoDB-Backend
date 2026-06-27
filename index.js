3ef9ce319a06e4a86309bce601353df1ef048ff5
NaniGudivaka <nanigudivaka0804@gmail.com>
2026-06-27 15:36:13 +0530

    Basic setup for backend

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
