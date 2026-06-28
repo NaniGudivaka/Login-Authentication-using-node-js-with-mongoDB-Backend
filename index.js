70a13258f35fe6fe516f01b293a5d3700250ef14
NaniGudivaka <nanigudivaka0804@gmail.com>
2026-06-28 21:27:35 +0530

    fixing bugs in cors origin

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ connectDB();
 app.use(express.json());
 app.use(cors({
   origin: ['http://127.0.0.1:5500',
-    'https://nanigudivaka.github.io/Login-authentication-using-MongoDB'],
+    'https://nanigudivaka.github.io'],
 
   credentials:true
 }));
